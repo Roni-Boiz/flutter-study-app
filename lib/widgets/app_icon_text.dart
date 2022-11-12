e8db60f0847f36d58b120069125db6d7d2e32452
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-11-12 15:01:35 +0530

    areate app icon text formats

---
+++ lib/widgets/app_icon_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class AppIconText extends StatelessWidget {
+  final Icon icon;
+  final Widget text;
+  const AppIconText({Key? key,
+    required this.icon,
+    required this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        icon,
+        const SizedBox(
+          width: 4,
+        ),
+        text
+      ],
+    );
+  }
+}
