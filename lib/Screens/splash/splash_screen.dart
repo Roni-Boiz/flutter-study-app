a49a4d4d3bf9d1e84050679c430f615afca0a631
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-11-12 14:55:41 +0530

    modify spalsh screen

--- lib/Screens/splash/splash_screen.dart
+++ lib/Screens/splash/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project/configs/themes/app_colors.dart';
 
+import '../../configs/themes/app_icons.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -9,11 +11,27 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        decoration: BoxDecoration(gradient: mainGradient(context)),
-        child: Image.asset("assets/images/app_splash_logo.png",
-          width: 200,
-          height: 200,
-        )
+        decoration: BoxDecoration(gradient: mainGradient()),
+        child: Column(
+          children: [
+            Spacer(),
+            Image.asset("assets/images/app_splash_logo.png",
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 40,),
+            Text(
+              "Study App",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: AppIcons.fontFam,
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
