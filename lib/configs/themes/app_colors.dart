c132952c62f0d34cc2254c4efe8b3dedf717ba34
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-11-12 14:44:09 +0530

    update main gradient and customscaffold colors

--- lib/configs/themes/app_colors.dart
+++ lib/configs/themes/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:project/configs/themes/app_dark_theme.dart';
 import 'package:project/configs/themes/ui_parameters.dart';
 import 'app_light_theme.dart';
+import 'package:get/get.dart';
 
 const Color onSurfaceTextColor = Colors.white;
 
@@ -24,5 +25,16 @@ const mainGradientDark = LinearGradient(
     ]
 );
 
-LinearGradient mainGradient(BuildContext context) =>
-  UIParaqmeters.isDarkMode(context) ? mainGradientDark : mainGradientLight;
+LinearGradient mainGradient() =>
+  UIParaqmeters.isDarkMode() ? mainGradientDark : mainGradientLight;
+
+customScaffoldColor(BuildContext context) =>
+    UIParaqmeters.isDarkMode()?const Color(0xFF2e3c62)
+        : const Color.fromARGB(255, 240, 237, 255);
+
+Color answerSelectedColor() => UIParaqmeters.isDarkMode() ? Theme.of(Get.context!).cardColor.withOpacity(0.5) :
+    Theme.of(Get.context!).primaryColor;
+
+Color answerBorderColor() => UIParaqmeters.isDarkMode() ?
+    const Color.fromARGB(255, 20, 46, 158) :
+    const Color.fromARGB(255, 221, 221, 221);
