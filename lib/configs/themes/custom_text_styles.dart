83147580179b0fe6a1f9dcf1a11ee7a239dbf097
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-11-14 23:25:41 +0530

    update font sized of appbar, question and header

--- lib/configs/themes/custom_text_styles.dart
+++ lib/configs/themes/custom_text_styles.dart
@@ -7,15 +7,15 @@ import 'package:get/get.dart';
 TextStyle cardTitles(context) => TextStyle(
   color: UIParaqmeters.isDarkMode()?Theme.of(context).textTheme.bodyText1!.color
       :Theme.of(context).primaryColor,
-  fontSize: 18,
+  fontSize: 20,
   fontWeight: FontWeight.bold,
 );
 
-const questionText = TextStyle(fontSize: 18, fontWeight: FontWeight.w800);
+const questionText = TextStyle(fontSize: 20, fontWeight: FontWeight.w800);
 const detailText = TextStyle(fontSize: 15,);
 const headerText = TextStyle(fontSize: 24, fontWeight: FontWeight.w700, color: onSurfaceTextColor);
 
-const appBarTS = TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: onSurfaceTextColor);
+const appBarTS = TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: onSurfaceTextColor);
 
 TextStyle coutdownTimerTs() => TextStyle(
   letterSpacing: 2,
