548bac04e2fa61e315ac84140fc6f78b0c7baa55
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-11-14 23:35:24 +0530

    create answer card

--- lib/widgets/questions/answer_card.dart
+++ lib/widgets/questions/answer_card.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:project/configs/themes/app_colors.dart';
 import 'package:project/configs/themes/ui_parameters.dart';
 
+enum AnswerStatus{
+  correct,
+  wrong,
+  answered,
+  notanswered,
+}
+
 class AnswerCard extends StatelessWidget {
 
   final String answer;
@@ -14,6 +21,8 @@ class AnswerCard extends StatelessWidget {
     this.isSelected = false,
     required this.onTap
   }) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +42,81 @@ class AnswerCard extends StatelessWidget {
           answer,
           style: TextStyle(
             color: isSelected?onSurfaceTextColor:null,
+            fontWeight: FontWeight.w500,
+            fontSize: 18
           ),
         ),
       ),
     );
   }
 }
+
+class CorrectAnswer extends StatelessWidget {
+  const CorrectAnswer({Key? key, required this.answer}) : super(key: key);
+  final String answer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: BoxDecoration(
+        borderRadius: UIParaqmeters.cardBorderRadius,
+        color: correctAnswerColor.withOpacity(0.1),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Text(
+        answer,
+        style: TextStyle(
+          color: correctAnswerColor,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+class WrongAnswer extends StatelessWidget {
+  const WrongAnswer({Key? key, required this.answer}) : super(key: key);
+  final String answer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: BoxDecoration(
+        borderRadius: UIParaqmeters.cardBorderRadius,
+        color: wrongAnswerColor.withOpacity(0.1),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Text(
+        answer,
+        style: const TextStyle(
+            color: wrongAnswerColor,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+class NotAnswer extends StatelessWidget {
+  const NotAnswer({Key? key, required this.answer}) : super(key: key);
+  final String answer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: BoxDecoration(
+        borderRadius: UIParaqmeters.cardBorderRadius,
+        color: notAnsweredColor.withOpacity(0.1),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Text(
+        answer,
+        style: const TextStyle(
+            color: notAnsweredColor,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
